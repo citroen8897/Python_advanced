f5da496ad136530899249719e9d881a05be37ec9
predator2508 <citroen8897@gmail.com>
2021-01-31 12:52:08 +0200

    отформатировано Black

--- Lecon_un/numero_2.py
+++ Lecon_un/numero_2.py
@@ -2,35 +2,44 @@
 
 
 def main():
-    user_numero = input('S.P.Q.R.\nЗадайте целое число для проверки: ')
-    with open('matrix.txt', 'r') as file_matrix:
+    user_numero = input("S.P.Q.R.\nЗадайте целое число для проверки: ")
+    with open("matrix.txt", "r") as file_matrix:
         matrix = [line.split() for line in file_matrix]
 
-    index_lines = [matrix.index(line) for line in matrix
-                   if user_numero in line]
+    index_lines = [
+        matrix.index(line) for line in matrix if user_numero in line
+    ]
 
-    matrix_transpose = [[line[j] for line in matrix] for j in
-                        range(len(matrix))]
+    matrix_transpose = [
+        [line[j] for line in matrix] for j in range(len(matrix))
+    ]
 
-    index_rows = [matrix_transpose.index(line) for line in matrix_transpose
-                  if user_numero in line]
+    index_rows = [
+        matrix_transpose.index(line)
+        for line in matrix_transpose
+        if user_numero in line
+    ]
 
     if len(index_lines) == 0:
-        print(f'Число {user_numero} в матрице отсутствует\n')
+        print(f"Число {user_numero} в матрице отсутствует\n")
     else:
-        print(f'Число {user_numero} встречается в строках: '
-              f'{", ".join(map(str, index_lines))}')
-        print(f'Число {user_numero} встречается в столбцах: '
-              f'{", ".join(map(str, index_rows))}')
+        print(
+            f"Число {user_numero} встречается в строках: "
+            f'{", ".join(map(str, index_lines))}'
+        )
+        print(
+            f"Число {user_numero} встречается в столбцах: "
+            f'{", ".join(map(str, index_rows))}'
+        )
 
-    continuer = input('\nПроверить другое число?\nД/н')
-    if continuer == 'Д':
+    continuer = input("\nПроверить другое число?\nД/н")
+    if continuer == "Д":
         print()
         main()
     else:
-        input('Нажмите любую клавишу')
+        input("Нажмите любую клавишу")
         exit()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- func_practice.py
+++ func_practice.py
@@ -14,14 +14,16 @@
 #
 # print(func_1(2, 3))
 
+
 def func_2(nom):
     def func_3(prenom):
-        print(f'Bonjour {nom} {prenom}')
+        print(f"Bonjour {nom} {prenom}")
+
     return func_3
 
 
 # func_2(input('Votre nom est: '), input('Votre prenom est: '))
-func_2(input('Votre nom est: '))(input('Votre prenom est: '))
+func_2(input("Votre nom est: "))(input("Votre prenom est: "))
 
-temp = func_2('Jerome')
-temp('Roten')
+temp = func_2("Jerome")
+temp("Roten")
